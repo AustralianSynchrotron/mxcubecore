ec1029028f89fccc526e172efa474c7c3971ce7d
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2025-06-03 11:39:26 +1000

    set cryo temp

--- mxcubecore/HardwareObjects/ANSTO/Cryo.py
+++ mxcubecore/HardwareObjects/ANSTO/Cryo.py
@@ -3,6 +3,7 @@ from mx3_beamline_library.devices.cryo import cryo_temperature
 from mxcubecore.configuration.ansto.config import settings
 from mxcubecore.HardwareObjects.abstract.AbstractMotor import AbstractMotor
 from mxcubecore.HardwareObjects.ANSTO.EPICSActuator import EPICSActuator
+import time
 
 
 class Cryo(AbstractMotor, EPICSActuator):
@@ -77,3 +78,44 @@ class Cryo(AbstractMotor, EPICSActuator):
             Position of the motor.
         """
         return cryo_temperature.get()
+    
+
+    def _set_value(self, value: float) -> None:
+        """Sets the new temperature
+
+        Parameters
+        ----------
+        value : float
+            Position of the motor.
+
+        Returns
+        -------
+        None
+        """
+        self.update_state(self.STATES.BUSY)
+        cryo_temperature.set(value)
+
+    def _move(self, value: float) -> float:
+        """Move the motor to a given value.
+
+        Parameters
+        ----------
+        value : float
+            Position of the motor.
+
+        Returns
+        -------
+        float
+            New position of the motor.
+        """
+        self.update_state(self.STATES.BUSY)
+        self.update_specific_state(self.SPECIFIC_STATES.MOVING)
+
+        while round(cryo_temperature.get(), 1) != round(value,1):
+            time.sleep(0.2)
+            self.update_value(self.get_value())
+
+        self.update_state(self.STATES.READY)
+        self.update_value(self.get_value())
+        return value
+
