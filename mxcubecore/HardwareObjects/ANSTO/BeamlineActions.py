84188713a42857507f1406717c0a01e3e6cd8557
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2025-06-06 15:17:59 +1000

    beamline actions work

--- mxcubecore/HardwareObjects/ANSTO/BeamlineActions.py
+++ mxcubecore/HardwareObjects/ANSTO/BeamlineActions.py
@@ -1,75 +1,162 @@
 import logging
+from time import perf_counter
 
 import gevent
-from pydantic.v1 import (
-    BaseModel,
-    Field,
-)
-from typing_extensions import Literal
+from mx3_beamline_library.devices.motors import detector_fast_stage
+from mx_robot_library.client.client import Client
+from mx_robot_library.schemas.common.path import RobotPaths
+from mx_robot_library.schemas.common.position import RobotPositions
 
-from mxcubecore.HardwareObjects.BeamlineActions import (
-    AnnotatedCommand,
-    BeamlineActions,
-)
-
-
-class SimpleFloat(BaseModel):
-    exp_time: float = Field(100e-6, gt=0, lt=10, description="(s)")
-
-
-class StringLiteral(BaseModel):
-    phase: Literal["Centring", "DataCollection", "BeamLocation", "Transfer"] = Field(1)
-
-
-class SimulatedAction:
-    def __call__(self, *args, **kw):
-        gevent.sleep(3)
-        return args
-
-
-class SimulatedActionError:
-    def __call__(self, *args, **kw):
-        raise RuntimeError("Simulated error")
-
-
-class LongSimulatedAction:
-    def __call__(self, *args, **kw):
-        for i in range(10):
-            gevent.sleep(1)
-            logging.getLogger("user_level_log").info("%d, sleeping for 1 second", i + 1)
-
-        return args
-
-
-class Anneal2(AnnotatedCommand):
-    def __init__(self, *args):
-        super().__init__(*args)
-
-    def anneal2(self, data: SimpleFloat) -> None:
-        logging.getLogger("user_level_log").info(
-            f"Annealing for {data.exp_time} seconds"
-        )
-        gevent.sleep(data.exp_time)
-
-
-class QuickRealign2(AnnotatedCommand):
-    def __init__(self, *args):
-        super().__init__(*args)
-
-    def quick_realign2(self) -> None:
-        for i in range(10):
-            gevent.sleep(1)
-            logging.getLogger("user_level_log").info("%d, sleeping for 1 second", i + 1)
-
-
-class ComboTest2(AnnotatedCommand):
-    def __init__(self, *args):
-        super().__init__(*args)
-
-    def combo_test2(self, data: StringLiteral) -> None:
-        logging.getLogger("user_level_log").info(f"Selected {data.phase}")
+from mxcubecore.configuration.ansto.config import settings
+from mxcubecore.HardwareObjects.BeamlineActions import BeamlineActions
 
 
 class BeamlineActions(BeamlineActions):
+    # For more examples, check the BeamlineActionsMockup class
     def __init__(self, *args):
         super().__init__(*args)
+
+
+class ParkRobot:
+    """Send the robot home and close the lid"""
+
+    def __call__(self, *args, **kw):
+        robot = RobotTrajectory()
+
+        robot.home()
+
+        robot.close_lid()
+
+        return args
+
+
+class DetectorBack:
+    """Send the the detector back"""
+
+    def __call__(self, *args, **kw):
+        self._move_detector_back()
+
+        return args
+
+    def _move_detector_back(self):
+        try:
+            logging.getLogger("user_level_log").info("Moving detector fast stage...")
+            detector_fast_stage.move(650, wait=False)
+
+            while detector_fast_stage.moving:
+                gevent.sleep(0.5)
+
+            logging.getLogger("user_level_log").info(
+                "Detector fast stage successfully moved back"
+            )
+        except Exception as e:
+            logging.getLogger("user_level_log").info(
+                f"Failed to move detector fast stage: {str(e)}"
+            )
+
+
+class EnterHutch:
+    """Send the robot home, close the lid, and send the detector back"""
+
+    def __call__(self, *args, **kw):
+        robot = RobotTrajectory()
+
+        robot.home()
+
+        robot.close_lid()
+
+        self._move_detector_back()
+
+        return args
+
+    def _move_detector_back(self):
+        try:
+            logging.getLogger("user_level_log").info("Moving detector fast stage...")
+            detector_fast_stage.move(650, wait=False)
+
+            while detector_fast_stage.moving:
+                gevent.sleep(0.5)
+
+            logging.getLogger("user_level_log").info(
+                "Detector fast stage successfully moved back"
+            )
+        except Exception as e:
+            logging.getLogger("user_level_log").info(
+                f"Failed to move detector fast stage: {str(e)}"
+            )
+
+
+class RobotTrajectory:
+    def __init__(self):
+        if settings.BL_ACTIVE:
+            self.robot_client = Client(host=settings.ROBOT_HOST, readonly=False)
+
+    def home(self):
+        logging.getLogger("user_level_log").info("Homing")
+        if settings.BL_ACTIVE:
+            try:
+                if self.robot_client.status.state.position == RobotPositions.HOME:
+                    logging.getLogger("user_level_log").info(
+                        "Robot is already in home position"
+                    )
+                    return
+                self.robot_client.trajectory.home()
+
+                self._wait_robot()
+
+                if self.robot_client.status.state.position != RobotPositions.HOME:
+                    raise ValueError(
+                        f"Current position is {self.robot_client.status.state.position}, not home"
+                    )
+
+                logging.getLogger("user_level_log").info("Homing completed")
+            except Exception as e:
+                logging.getLogger("user_level_log").info(
+                    f"Failed to change the robot position to home: {str(e)}"
+                )
+        else:
+            gevent.sleep(2)  # Simulate some processing time
+            logging.getLogger("user_level_log").info("Homing completed")
+
+    def close_lid(self):
+        logging.getLogger("user_level_log").info("Homing")
+        if settings.BL_ACTIVE:
+            try:
+
+                response = self.robot_client.common.close_lid()
+                if response.error is not None:
+                    logging.getLogger("user_level_log").info(
+                        f"Failed to close lid: {response.error}"
+                    )
+                    return
+
+                logging.getLogger("user_level_log").info("Lid successfully closed")
+            except Exception as e:
+                logging.getLogger("user_level_log").info(
+                    f"Failed to close the robot lid {str(e)}"
+                )
+        else:
+            gevent.sleep(2)  # Simulate some processing time
+            logging.getLogger("user_level_log").info("Lid successfully closed")
+
+    def _wait_robot(self, timeout=120):
+        """
+        Waits until the robot has finished running a trajectory
+
+        Parameters
+        ----------
+        timeout : float, optional
+            The timeout in seconds, by default 120
+
+        Raises
+        ------
+        ValueError
+            Raises an error if the path cannot be changed after timeout seconds
+        """
+        _timeout = perf_counter() + timeout
+        gevent.sleep(0.5)
+
+        while self.robot_client.status.state.path != RobotPaths.UNDEFINED:
+            gevent.sleep(0.5)
+            if perf_counter() >= _timeout:
+                raise ValueError(f"Could not change robot path after {timeout} seconds")
