db89b34bee0287170964066211ed09bda7dfcc18
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2025-06-23 20:04:43 +1000

    fix typo

--- mxcubecore/HardwareObjects/ANSTO/prefect_flows/screening_flow.py
+++ mxcubecore/HardwareObjects/ANSTO/prefect_flows/screening_flow.py
@@ -119,14 +119,14 @@ class ScreeningFlow(AbstractPrefectWorkflow):
             "exposure_time": {
                 "title": "Total Exposure Time [s]",
                 "type": "number",
-                " exclusiveMinimum": 0,
+                "exclusiveMinimum": 0,
                 "default": float(self._get_dialog_box_param("exposure_time")),
                 "widget": "textarea",
             },
             "omega_range": {
                 "title": "Omega Range [degrees]",
                 "type": "number",
-                " exclusiveMinimum": 0,
+                "exclusiveMinimum": 0,
                 "default": float(self._get_dialog_box_param("omega_range")),
                 "widget": "textarea",
             },
