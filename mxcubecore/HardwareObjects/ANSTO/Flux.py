914d37ccc696d241b613718aff98220d40655398
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2025-06-24 17:53:16 +1000

    add flux

---
+++ mxcubecore/HardwareObjects/ANSTO/Flux.py
@@ -0,0 +1,42 @@
+from mx3_beamline_library.devices.beam import flux
+
+from mxcubecore import HardwareRepository as HWR
+from mxcubecore.configuration.ansto.config import settings
+from mxcubecore.HardwareObjects.abstract.AbstractFlux import AbstractFlux
+
+
+class Flux(AbstractFlux):
+    """Class to simulate beamline flux"""
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def init(self):
+        super().init()
+
+        if settings.BL_ACTIVE:
+            # The following channel is used to poll the flux PV value
+            self.energy_channel = self.add_channel(
+                {
+                    "type": "epics",
+                    "name": "flux",
+                    "polling": 2000,  # milliseconds
+                },
+                flux.pvname,
+            )
+            self.energy_channel.connect_signal("update", self._value_changed)
+
+    def _value_changed(self, value: float | None) -> None:
+        """Emits a valueChanged signal. Used by self.energy_channel
+
+        Parameters
+        ----------
+        value : float | None
+            The energy value
+        """
+        self._value = value
+        self.emit("valueChanged", self._value)
+
+    def get_value(self):
+        """Get flux in units of photons/s"""
+        return flux.get()
