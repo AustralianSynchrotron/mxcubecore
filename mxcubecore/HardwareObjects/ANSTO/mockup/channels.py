f72f65021c1379e20a1c0b566af11032d7f556d3
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2025-07-22 15:04:39 +1000

    pre-commit

--- mxcubecore/HardwareObjects/ANSTO/mockup/channels.py
+++ mxcubecore/HardwareObjects/ANSTO/mockup/channels.py
@@ -1,12 +1,13 @@
 class SimChannel:
     """Only used for SIM mode"""
+
     def __getattr__(self, name):
         # Return a dummy function for any method
         def dummy(*args, **kwargs):
             return None
 
         return dummy
-    
+
 
 class SimMd3State(SimChannel):
     """Only used for SIM mode"""
@@ -14,10 +15,9 @@ class SimMd3State(SimChannel):
     def get_value(self, *args, **kwargs):
         return "Ready"
 
+
 class SimMd3Phase(SimChannel):
     """Only used for SIM mode"""
 
     def get_value(self, *args, **kwargs):
         return "DataCollection"
-    
-
