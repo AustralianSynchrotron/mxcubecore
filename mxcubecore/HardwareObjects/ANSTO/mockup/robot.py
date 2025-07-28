cdb3aa855c0a409d44e33a580bce8ca3b2510b5f
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2025-07-28 13:33:36 +1000

    mockup more robot methods

--- mxcubecore/HardwareObjects/ANSTO/mockup/robot.py
+++ mxcubecore/HardwareObjects/ANSTO/mockup/robot.py
@@ -1,3 +1,5 @@
+from gevent import sleep
+from mx_robot_library.schemas.common.position import RobotPositions
 from mx_robot_library.schemas.common.sample import (
     Pin,
     Plate,
@@ -5,10 +7,22 @@ from mx_robot_library.schemas.common.sample import (
 )
 
 
+class MockPlateTrajectory:
+    def mount(self, plate, wait=True):
+        print(f"Mock mount called with plate: {plate}, wait: {wait}")
+        sleep(2)
+
+
+class MockTrajectory:
+    def __init__(self):
+        self.plate = MockPlateTrajectory()
+
+
 class SimState:
     def __init__(self):
         self.goni_plate = None
         self.goni_pin = None
+        self.path = RobotPositions.SOAK
 
 
 class SimStatus:
@@ -32,9 +46,17 @@ class SimStatus:
         )
 
 
+class MockCommon:
+    def abort(self):
+        print("Mock abort called")
+        return True
+
+
 class SimRobot:
     def __init__(self):
         self.status = SimStatus()
+        self.trajectory = MockTrajectory()
+        self.common = MockCommon()
 
 
 if __name__ == "__main__":
@@ -44,3 +66,6 @@ if __name__ == "__main__":
     print(robot.status.state.goni_plate)
     print(robot.status.get_loaded_pucks())
     print(robot.status.state.goni_pin)
+    print(robot.trajectory.plate.mount(Plate(id=1)))
+    print(robot.status.state.path)
+    robot.common.abort()
