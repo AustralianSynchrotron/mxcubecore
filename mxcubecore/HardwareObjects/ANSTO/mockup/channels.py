805a50b1ddfd8790862e8eb9bf40a2f6e830b563
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2025-08-13 12:08:37 +1000

    sim channel now implements get and set value methods

--- mxcubecore/HardwareObjects/ANSTO/mockup/channels.py
+++ mxcubecore/HardwareObjects/ANSTO/mockup/channels.py
@@ -1,12 +1,29 @@
 class SimChannel:
     """Only used for SIM mode"""
 
+    def __init__(self, name: str = "sim_channel", initial_value=None):
+        self._name = name
+        self._value = initial_value
+
     def __getattr__(self, name):
         # Return a dummy function for any method
         def dummy(*args, **kwargs):
             return None
 
         return dummy
+
+    def set_value(self, value):
+        self._value = value
+
+    def get_value(self):
+        return self._value
+
+    def __call__(self, value=None):
+        self.set_value(value)
+        return self._value
+
+    def name(self):
+        return self._name
 
 
 class SimMd3State(SimChannel):
